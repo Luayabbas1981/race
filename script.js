340fd5a0658c07559b9880eca403f52b38552dd7
Luay Abbas <abbasluay1981@gmail.com>
2023-11-11 13:41:17 +0100

    init-commit

--- script.js
+++ script.js
@@ -10,58 +10,66 @@ const scaleFactor = window.devicePixelRatio;
 canvas.width = canvas.clientWidth * scaleFactor;
 canvas.height = canvas.clientHeight * scaleFactor;
 context.scale(scaleFactor, scaleFactor);
-// Set screen dimensions
-const screenHeight = window.innerHeight;
-const screenWidth = window.innerWidth;
-// Set ballArea dimensions
-const raceAreaHeight = raceArea.offsetHeight;
-const raceAreaWidth = raceArea.offsetWidth;
-// Get canvas position in page
-const canvasRect = canvas.getBoundingClientRect();
-// Add game images
-const ball = new Image();
-ball.src = "./images/ball.png";
-let ballX = canvas.width / 2;
-let ballY = canvas.height - 60;
-const firstHole = new Image();
-firstHole.src = "./images/hole.png";
-const secondHole = new Image();
-secondHole.src = "./images/hole.png";
-const thirdHole = new Image();
-thirdHole.src = "./images/hole.png";
-const fourthHole = new Image();
-fourthHole.src = "./images/hole.png";
-const fifthHole = new Image();
-fifthHole.src = "./images/hole.png";
 
-window.onload = function () {
-  context.drawImage(ball, ballX, ballY, 50, 50);
-  context.drawImage(firstHole, canvas.width/10, canvas.height/16, canvas.width/10, canvas.height/12);
-  context.drawImage(secondHole, canvas.width/2.3, canvas.height/16, canvas.width/10, canvas.height/12)
-  context.drawImage(thirdHole, canvas.width/1.3, canvas.height/16, canvas.width/10, canvas.height/12);
-  context.drawImage(fourthHole, canvas.width/6, canvas.height/5, canvas.width/10, canvas.height/12);
-  context.drawImage(fifthHole, canvas.width/2, canvas.height/5, canvas.width/10, canvas.height/12); 
-};
-//Ball position
- 
 // Set speed
-/* function setSpeed() {
+ function setSpeed() {
   ballSpeed++;
-  console.log("ballSpeed", ballSpeed *3);
+  
 }
-canvas.addEventListener("pointerdown", function (event) {
-  ballSpeedInterval = setInterval(setSpeed, 1);
-  mouseX = event.clientX - canvasRect.left;
-  mouseY = event.clientY - canvasRect.top;
-  console.log("mouseX", mouseX, ballX);
-  console.log("mouseY", mouseY, ballY);
-});
-canvas.addEventListener("pointerup", function () {
-  clearInterval(ballSpeedInterval);
-  function animate() {
-    requestAnimationFrame(animate);
-    context.clearRect(0, 0, raceAreaWidth, raceAreaHeight);
-    context.drawImage(ball, ballX, -ballY, 50, 50);
+// Ball class
+
+
+// Hole class
+class Hole {
+  constructor(holeSrc, x, y, width, height) {
+    this.hole = new Image();
+    this.hole.src = holeSrc;
+    this.hole.onload = () => {
+      this.draw(); // Call draw() once the image is loaded
+    };
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.id = this.id;
+    this.rect = this.rect;
   }
-});
- */
+
+  draw() {
+    context.drawImage(this.hole, this.x, this.y, this.width, this.height);
+  }
+}
+let holeSArray =[]
+function createHoles() {
+  for (let index = 0; index < 8; index++) {
+    if(index===0){
+      let hole = new Hole("./images/hole.png", 60, 50, 80, 70);
+      hole.id = index;
+      holeSArray.push(hole)
+    }
+    if(index===1 || index===2){
+      let hole = new Hole("./images/hole.png", index  * 280, 50, 80, 70);
+      hole.id = index;
+      holeSArray.push(hole)
+    }
+    if(index===3){
+      let hole = new Hole("./images/hole.png", 90, 140, 80, 70);
+      hole.id = index;
+      holeSArray.push(hole)
+    }
+    if(index===4 || index===5 ){
+      let hole = new Hole("./images/hole.png", (index -3) *270,160, 80, 70);
+      hole.id = index;
+      holeSArray.push(hole)
+    }
+    if(index===6 || index===7 ){
+      let hole = new Hole("./images/hole.png", (index -5) *200,270, 80, 70);
+      hole.id = index;
+      holeSArray.push(hole)
+    }
+   
+  }
+}
+createHoles();
+
+
